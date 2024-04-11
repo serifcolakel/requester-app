8ecd795673a08aff86ef42053ced27848a46aa1b
Serif <serifcolakel0@gmail.com>
2024-04-12 01:34:58 +0300

    Update page component to fetch collection and request data

--- app/dashboard/collections/[...id]/page.tsx
+++ app/dashboard/collections/[...id]/page.tsx
@@ -1,3 +1,27 @@
+import Requests from "@/components/screens/requests";
+import { getCollectionById } from "@/services/collections";
+import { getRequest } from "@/services/requests";
+import { Nullable } from "@/types";
+import { Request } from "@prisma/client";
+
 export default async function Page({ params }: { params: { id: string[] } }) {
-  return <div className="p-4">My Collection {params.id.join("-------")}</div>;
+  const [collectionId, requestId] = params.id;
+
+  const { data: collection } = await getCollectionById(collectionId);
+
+  let request: Nullable<Request> = null;
+
+  if (requestId) {
+    const response = await getRequest(requestId);
+
+    if (response.data) request = response.data;
+  }
+
+  return (
+    <div className="p-4">
+      {request && collection ? (
+        <Requests collection={collection} request={request} />
+      ) : null}
+    </div>
+  );
 }

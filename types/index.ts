68b85d953c4c7c402962ff23fb444d8eafe7cf6f
Serif <serifcolakel0@gmail.com>
2024-04-16 00:58:31 +0300

    Add types for Test Handler and Test Result

--- types/index.ts
+++ types/index.ts
@@ -12,3 +12,25 @@ export type BaseServiceResponse<T> = {
  * @description: Nullable type
  */
 export type Nullable<T> = T | null;
+
+/**
+ * @description: Test Handler type
+ */
+export type Handler = {
+  environment: {
+    set: (key: string, value: unknown) => void;
+    replace: (key: string, value: unknown) => void;
+    get: (key: string) => unknown;
+    unset: (key: string) => void;
+  };
+};
+
+/**
+ * @description: Test result type
+ */
+export type TestResult = {
+  expected: unknown;
+  actual: unknown;
+  passed: boolean;
+  resultMessage: string;
+};
